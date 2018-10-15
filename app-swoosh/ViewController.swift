14d769e715c931f43f3002053353dcf40adef2d1
nelsonaudio <jeff@nelsonmediasystems.com>
2018-10-15 11:23:27 -0700

    AutoLayout and Constraints

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -15,11 +15,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        // MANUAL setup for pixel perfect design.  Don't do this!!  Use Auto-Layout and Constraints!!
-        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
-        // math to get swoosh image to fit within
-        bgImg.frame = view.frame
+        
     }
     
     override func didReceiveMemoryWarning() {
