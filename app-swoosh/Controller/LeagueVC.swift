8d73840a2a5895f5b8406fd13cf36a3c437bd29d
nelsonaudio <jeff@nelsonmediasystems.com>
2018-10-16 11:52:56 -0700

    IBActions and data models

--- app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/Controller/LeagueVC.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class LeagueVC: UIViewController {
     
+    // Model that can store and pass data called Player
+    var player:  Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // initialize the struct, when viewDidLoad occurs, it's going to create a new player
+        player = Player()
     }
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    //Try to never have logic built into IBAction, instead create a function and pass that function into the IBAction like above and below
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType    // pass in the league type
+        nextBtn.isEnabled = true
+    }
+    
+    
     
     
     /*
---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Jeff Nelson on 10/16/18.
+//  Copyright © 2018 Nelson Media Systems. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
